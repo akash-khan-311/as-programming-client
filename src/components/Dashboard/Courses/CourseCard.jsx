24d7eb299768deb3d050561f12f9926452801eb0
Akash Khan <akashcc48@gmail.com git config --global user.email akashcc48@gmail.com git config --global user.email akashcc48@gmail.com>
2024-07-25 15:05:49 +0600

    assignment modal implemented

--- src/components/Dashboard/Courses/CourseCard.jsx
+++ src/components/Dashboard/Courses/CourseCard.jsx
@@ -1,25 +1,34 @@
 "use client";
-
 import { getSingleCourse } from "@/api/courses";
-import Loader from "@/components/Shared/Loader";
+import Field from "@/components/Shared/Form/Field";
 import Image from "next/image";
-import Link from "next/link";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { useQuery } from "react-query";
 
 const CourseCard = ({ id }) => {
-  console.log(id);
+  const [modalOpen, setModalOpen] = useState(false);
   const {
     data: singleCourse,
     refetch,
     isLoading,
   } = useQuery(["course", id], async () => await getSingleCourse(id), {
-    enabled: !!id, // Only fetch if user email is available
+    enabled: !!id,
   });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitForm = async (formData) => {
+    console.log(formData);
+  };
 
   if (isLoading) {
     return <div className="text-4xl text-white">Course Comming.........</div>;
   }
-  console.log(singleCourse);
 
   return (
     <div className=" mx-auto">
@@ -40,10 +49,59 @@ const CourseCard = ({ id }) => {
             <button className="w-full transition-all duration-200 bg-pink-600 px-10 py-2 rounded-lg hover:bg-pink-500">
               Continue Course
             </button>
-            <button className="w-full transition-all duration-200 bg-pink-600 px-10 py-2 rounded-lg hover:bg-pink-500">
+            <button
+              onClick={() => setModalOpen(true)}
+              className="w-full transition-all duration-200 bg-pink-600 px-10 py-2 rounded-lg hover:bg-pink-500"
+            >
               Submit Assignment
             </button>
           </div>
+        </div>
+      </div>
+      <div
+        onClick={() => setModalOpen(false)}
+        className={`z-[999] fixed flex items-center justify-center h-screen w-screen place-items-center ${
+          modalOpen ? "visible opacity-1" : "invisible opacity-0"
+        } inset-0 bg-black bg-opacity-60 duration-100 `}
+      >
+        <div
+          onClick={(e) => e.stopPropagation()}
+          className={`absolute m-4 w-3/4 min-w-[60%] max-w-[60%] rounded-lg backdrop-blur-3xl bg-white/30 text-white font-sans text-base font-lightd  ${
+            modalOpen
+              ? "translate-y-6 opacity-1 duration-300"
+              : "-translate-y-6 opacity-0 duration-200"
+          } shadow-2xl`}
+        >
+          <div className="flex items-center p-4 font-sans text-2xl antialiased font-semibold leading-snug shrink-0 text-blue-gray-900">
+            {singleCourse.title}
+          </div>
+          <div className="relative p-4 font-sans  antialiased font-semibold text-xl leading-relaxed border-t border-b border-t-blue-gray-100 border-b-blue-gray-100 ">
+            Write Assignment GitHub Repository Link...
+          </div>
+          <form className="p-5" onSubmit={handleSubmit(submitForm)} action="">
+            <Field required={true} error={errors.assignment}>
+              <textarea
+                {...register("assignment", {
+                  required: "Field is Empty",
+                  minLength: 20,
+                })}
+                className={`backdrop-blur-sm bg-white/10 border focus:outline-none p-5 w-full text-lg placeholder:text-white ${
+                  !!errors.assignment ? "border-red-500" : "border-white "
+                } `}
+                name="assignment"
+                rows={10}
+                id="assignment"
+                placeholder="live_link: your_github_repository_url"
+              ></textarea>
+            </Field>
+            <div role="alert" className="text-red-500"></div>
+            <button
+              className="bg-pink-600 hover:bg-pink-500 w-2/3 mx-auto flex justify-center items-center rounded-lg py-2 mb-10 mt-3"
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
         </div>
       </div>
     </div>
