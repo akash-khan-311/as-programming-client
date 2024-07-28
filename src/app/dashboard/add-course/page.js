affc701d9bdb52cf0d210259e36627a808e50436
Akash Khan <akashcc48@gmail.com git config --global user.email akashcc48@gmail.com git config --global user.email akashcc48@gmail.com>
2024-07-28 23:23:07 +0600

    some style changes

--- src/app/dashboard/add-course/page.js
+++ src/app/dashboard/add-course/page.js
@@ -19,7 +19,7 @@ export const metadata = {
 
 const AddCoursePage = () => {
   return (
-    <div>
+    <div className="mt-10 md:mt-0">
       <AddCourseCom />
     </div>
   );
--- src/components/Dashboard/TeacherAssignment/TeacherAssignmentCard.jsx
+++ src/components/Dashboard/TeacherAssignment/TeacherAssignmentCard.jsx
@@ -9,19 +9,19 @@ const TeacherAssignmentCard = ({ assignment, refetch }) => {
   console.log(assignment);
   const formattedDate = convertTimestampToDate(assignment.timestamp);
   return (
-    <div className="backdrop-blur-sm rounded-lg bg-white/10 ">
-      <div className="flex gap-x-10 ">
-        <div className="w-1/3">
+    <div className="backdrop-blur-2xl rounded-lg bg-white/10 ">
+      <div className="flex flex-col md:flex-row md:gap-x-5 gap-0 ">
+        <div className="md:w-1/3">
           <Image
             src={assignment?.courseImg}
             width={500}
             height={500}
-            className="w-full h-full rounded-l-lg"
+            className="w-full h-full rounded-t-lg md:rounded-l-lg"
             alt={assignment?.courseName}
           />
         </div>
 
-        <div className="w-2/3 py-5">
+        <div className="w-2/3 p-5 ">
           <h2 className="text-white text-xl md:text-2xl lg:text-3xl whitespace-no-wrap ">
             {assignment?.courseName}
           </h2>
@@ -36,16 +36,20 @@ const TeacherAssignmentCard = ({ assignment, refetch }) => {
             <li>
               Mark: {assignment?.mark ? assignment?.mark : "Comming Soon"}
             </li>
-            <li>
-              <button
-                onClick={isOpenDialog}
-                className="bg-pink-500 hover:bg-pink-700 w-40 text-white font-bold py-2 px-4 rounded"
-              >
-                View
-              </button>
-            </li>
+            <button
+              onClick={isOpenDialog}
+              className="bg-pink-500 hidden md:block  mb-5  hover:bg-pink-700 md:w-40 text-white font-bold py-2 px-4 rounded"
+            >
+              {assignment?.mark > 0 ? "Viewed" : "View"}
+            </button>
           </ul>
         </div>
+        <button
+          onClick={isOpenDialog}
+          className="bg-pink-500 md:hidden w-3/4 mx-auto mb-5  hover:bg-pink-700 md:w-40 text-white font-bold py-2 px-4 rounded"
+        >
+          {assignment?.mark > 0 ? "Viewed" : "View"}
+        </button>
       </div>
       <Modal
         assignment={assignment}
